27bf2f77d3bb9ed0049763b16facca367ae8524b
Sarah Cross <scc0079@auburn.edu>
2024-08-20 17:18:12 -0700

    Adding js to implement swipe for carousel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,6 +8,34 @@ import "bootstrap";
 //= require bootstrap-sprockets
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  const videoCarousel = document.getElementById('videoCarousel');
+  let startX = 0;
+  let endX = 0;
+
+  // Detect the start of a swipe
+  videoCarousel.addEventListener('touchstart', function (e) {
+    startX = e.touches[0].clientX;
+  });
+
+  // Detect the end of a swipe
+  videoCarousel.addEventListener('touchmove', function (e) {
+    endX = e.touches[0].clientX;
+  });
+
+  // Determine the direction of the swipe and trigger the appropriate carousel control
+  videoCarousel.addEventListener('touchend', function () {
+    if (startX - endX > 50) {
+      // Swipe left - go to the next slide
+      bootstrap.Carousel.getInstance(videoCarousel).next();
+    } else if (endX - startX > 50) {
+      // Swipe right - go to the previous slide
+      bootstrap.Carousel.getInstance(videoCarousel).prev();
+    }
+    startX = 0;
+    endX = 0;
+  });
+});
 
 
 
